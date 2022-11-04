6ca3ff3ed92d059c609c033aadcf7a786781db57
Basarat <basaratali@gmail.com>
2022-11-04 14:05:46 +1100

    wrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
-function App() {
-  return (<>
-    </>);
-}
+import { proxy, useSnapshot } from 'valtio';
+import { saveInfo } from './api';
 
-export default App;
+const state = proxy({
+  count: 0,
+  setCount: (count: number) => {
+    state.count = count;
+  },
+  inc: () => {
+    state.count++;
+  }
+});
+
+export default function App() {
+  const snap = useSnapshot(state);
+
+  async function onClick() {
+    await saveInfo();
+    state.inc();
+  }
+
+  return (
+    <>
+      <h1>Count: {snap.count}</h1>
+      <button onClick={onClick}>+ 1</button>
+    </>
+  );
+}
